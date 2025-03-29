dd9accf6b4bd69d4e0752132d03d992591633fa5
Yu Zhang <yu.zhang.yz862@yale.edu>
2025-03-29 22:34:42 +0800

    basic cursor movement

--- src/main/c.c
+++ src/main/c.c
@@ -19,6 +19,9 @@ void enable_raw_mode(void) {
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
